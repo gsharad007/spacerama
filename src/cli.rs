262455b6611721717d26b71c51834a3e41a59a65
gsharad007 <gsharad007@hotmail.com>
2024-05-22 21:02:29 -0400

    Adding bevy ggrs for p2p networking.

--- src/cli.rs
+++ src/cli.rs
@@ -2,7 +2,15 @@ use bevy::ecs::system::Resource;
 use clap::Parser;
 
 #[derive(Parser, Resource, Debug, Clone)]
+#[command(version, about, long_about = None)]
 pub struct CommandLineArguments {
+    /// the session id for current p2p session
+    #[clap(long, default_value = "spacerama")]
+    pub session_id: String,
+    /// the number of players for current p2p session
+    #[clap(long, default_value = "1")]
+    pub player_count: u8,
+
     /// runs the game in synctest mode
     #[clap(long)]
     pub synctest: bool,
